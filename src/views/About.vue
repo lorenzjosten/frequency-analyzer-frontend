3f40c937d58bf68f5640dd2b48cd76fc9ef3dd81
lorenz <lorenz.josten@gmail.com>
2022-12-01 14:11:36 +0000

    feat: add text to About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about d-flex h-100 flex-column">
+    <h1 id="title">Work sample</h1>
+    <p class="text-start">
+      This work sample consists of a reactive Spring Boot backend application with a vue.js frontend. It can be used to
+      upload and play a mp3 media file. Also, the power spectrum of the current track is shown.
+      Uploaded media files are persisted in a file based h2 database in your local app folder. It can be deleted safely
+      once the app has shut down. Used technologies include:
+    </p>
+
+    <ul class="list-group text-start">
+      <li class="list-group-item">reactive programming</li>
+      <li class="list-group-item">data streaming</li>
+      <li class="list-group-item">server sent events</li>
+      <li class="list-group-item">spring boot</li>
+      <li class="list-group-item">kotlin</li>
+      <li class="list-group-item">vue.js</li>
+      <li class="list-group-item">typescript</li>
+      <li class="list-group-item">gitlab pipelining</li>
+      <li class="list-group-item">liquibase</li>
+    </ul>
   </div>
 </template>
+
+<style scoped>
+#title {
+  color: #22ba58;
+}
+</style>
