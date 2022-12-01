be9abba92472c797992834dfc77aaaca553a9186
lorenz <lorenz.josten@gmail.com>
2022-12-01 14:13:13 +0000

    feat: drop unused styles

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,64 @@
 <script setup lang="ts"></script>
 
 <template>
+
   <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/"
-          ><img id="brand" src="@/assets/spectrum.png" alt="Power Spectrum"
-        /></a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <a class="navbar-brand" href="/"><img id="brand" src="@/assets/spectrum.png" alt="Power Spectrum" /></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <router-link class="nav-link" to="/">Home</router-link>
+              <router-link class="nav-link text-white" to="/">home</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/about">About</router-link>
+              <router-link class="nav-link text-white" to="/about">about</router-link>
             </li>
           </ul>
         </div>
       </div>
     </nav>
   </header>
-  <body>
-    <router-view />
-  </body>
+
+  <main class="flex-shrink-0">
+    <div class="container">
+      <router-view />
+    </div>
+  </main>
+
+  <footer class="footer mt-auto py-5 bg-dark">
+    <div class="container d-flex justify-content-end">
+      <a href="https://gitlab.com/lj-work-sample" class="text-white text-decoration-underline">sources on gitlab</a>
+    </div>
+  </footer>
+
 </template>
 
-<style>
+<style scoped>
 #brand {
   width: 30px;
   height: auto;
 }
+
 .router-link-active.nav-link {
+  color: #22ba58 !important;
   font-weight: bold;
 }
+
 .router-view {
   align-items: center;
   text-align: center;
+}
+
+body {
+  height: 100%;
+}
+
+header {
+  margin-bottom: 1em;
 }
 </style>
